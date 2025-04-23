f31f36eac8fc046763ac2134c49b13a048b10a69
Aida_Yuka <K024G0052@g.neec.ac.jp>
2025-04-23 16:56:17 +0900

    00-04

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <Windows.h>
 #include <cstdint>
+#include <string>
+#include <format>
 
 //ウィンドウプロシージャ
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
@@ -17,6 +19,54 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 	//標準のメッセージ処理を行う
 	return DefWindowProc(hwnd, msg, wparam, lparam);
 }
+
+void Log(const std::string& message)
+{
+	OutputDebugStringA(message.c_str());
+}
+
+std::wstring ConvertString(const std::string& str)
+{
+	if (str.empty())
+	{
+		return std::wstring();
+	}
+
+	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
+
+	if (sizeNeeded == 0)
+	{
+		return std::wstring();
+	}
+	std::wstring result(sizeNeeded, 0);
+	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
+
+	return result;
+}
+
+std::string ConvertString(const std::wstring& str)
+{
+	if (str.empty())
+	{
+		return std::string();
+	}
+
+	auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
+
+	if (sizeNeeded == 0)
+	{
+		return std::string();
+	}
+
+	std::string result(sizeNeeded, 0);
+	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
+
+	return result;
+}
+
+//ConvertString
+//std::wstring ConvertString(const std::string& str);
+//std::string ConvertString(const std::wstring& str);
 
 //Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
@@ -69,6 +119,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 	MSG msg{};
 
+	//文字列を格納する
+	std::string str0("STRING!!!");
+
+	//整列を文字列にする
+	std::string str1{ std::to_string(10) };
+
+	//文字の出力
+	Log(ConvertString(std::format(L"WSTRING{}\n", L"abc")));
+
 	//ウィンドウの×ボタンが押されるまでループ
 	while (msg.message != WM_QUIT)
 	{
@@ -81,6 +140,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		else
 		{
 			//ゲームの処理
+
+
 		}
 	}
 
